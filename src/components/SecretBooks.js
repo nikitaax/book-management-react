de3dc797755cb8e2c4714113ce97c8d72269e45e
nikitaax <nikkitanandankar@gmail.com>
2025-05-15 13:34:42 +0530

    private routes

---
+++ src/components/SecretBooks.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function SecretBooks() {
+  return (
+    <div>
+      <ul>
+        <li>Book 1</li>
+        <li>Book 2</li>
+        <li>Book 3</li>
+        <li>Book 4</li>
+        <li>Book 5</li>
+      </ul>
+    </div>
+  );
+}
